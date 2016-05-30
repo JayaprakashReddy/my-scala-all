0a314e637e6bb7dfd16fb279d327ae889fac5c24
jayaprakash.reddy <Jayaprakash.Reddy@digitalalchemy.com.au>
2016-05-30 23:11:23 +1000

    first

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := """my-scala-all"""
+
+version := "1.0"
+
+scalaVersion := "2.10.5"
+
+libraryDependencies += "org.scalacheck" % "scalacheck_2.10" % "1.13.0"
+
+resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases"
+
+
---
+++ src/main/scala/com/digitalalchemy/analytics/udiscover/UDiscoverApplication.scala
@@ -0,0 +1,23 @@
+package com.digitalalchemy.analytics.udiscover
+
+object UDiscoverApplication extends App {
+
+  println("*******************************************************")
+  println("This is uDiscover application")
+  println("Step1 : Variables creation")
+  println("Step2 : Variables reduction")
+  println("Step3 : Analytical Moedelling")
+  println("Step4 : Insights generation")
+  println("Step5 : Done")
+  println("*******************************************************")
+
+  def createVariables={
+   val s= List[String]("My","Name")
+   println(s.size)
+
+
+
+  }
+
+
+}
